0c0500c1744a04951f225f5fe3b5cd588eeb3c05
lucia <lucia.baquero3096@gmail.com>
2026-05-11 20:13:56 +0200

    Initial commit

---
+++ src/main/java/org/filmdex/filmdexbe/FilmdexBeApplication.java
@@ -0,0 +1,13 @@
+package org.filmdex.filmdexbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmdexBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmdexBeApplication.class, args);
+    }
+
+}
