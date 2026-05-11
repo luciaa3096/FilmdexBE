1d25f137ec14eaef3067fd9520c891e019bfae24
lucia <lucia.baquero3096@gmail.com>
2026-05-11 20:35:03 +0200

    user logic v1

---
+++ src/main/java/org/filmdex/filmdexbe/controllers/UserController.java
@@ -0,0 +1,39 @@
+package org.filmdex.filmdexbe.controllers;
+
+import org.filmdex.filmdexbe.models.User;
+import org.filmdex.filmdexbe.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
