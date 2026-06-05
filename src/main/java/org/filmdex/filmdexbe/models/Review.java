6cfdb6727f3ce216c5918d53ce2dc7faf19819db
lucia <lucia.baquero3096@gmail.com>
2026-06-05 18:17:21 +0200

    cors implementation and review import fix

--- src/main/java/org/filmdex/filmdexbe/models/Review.java
+++ src/main/java/org/filmdex/filmdexbe/models/Review.java
@@ -1,7 +1,6 @@
 package org.filmdex.filmdexbe.models;
 import jakarta.persistence.*;
 import lombok.Data;
-import org.apache.catalina.User;
 
 @Entity
 @Table(name = "reviews", uniqueConstraints = {
